716fc9cec7eb5cfa5d73671f6fd633837b034a39
carl0801 <118434635+carl0801@users.noreply.github.com>
2023-04-28 09:33:56 +0200

    Update Enviroment.py

--- simulation/Enviroment.py
+++ simulation/Enviroment.py
@@ -1,30 +1,60 @@
 
 import Jaco2_erobot
 import swift as swift
-from MQTT import MqttClient
+from Simulation_MQTT import MqttClient
 import sys
+import asyncio
+import numpy as np
+
+
+current_theta_topics = [
+            "P2/AAU/CURRENT/theta1",
+            "P2/AAU/CURRENT/theta2",
+            "P2/AAU/CURRENT/theta3",
+            "P2/AAU/CURRENT/theta4",
+            "P2/AAU/CURRENT/theta5",
+            "P2/AAU/CURRENT/theta6",
+            "P2/AAU/CURRENT/finger1",
+            "P2/AAU/CURRENT/finger2",
+            "P2/AAU/CURRENT/finger3",
+            ]
+simulation_topics = [
+            "P2/AAU/Simulation/theta1",
+            "P2/AAU/Simulation/theta2",
+            "P2/AAU/Simulation/theta3",
+            "P2/AAU/Simulation/theta4",
+            "P2/AAU/Simulation/theta5",
+            "P2/AAU/Simulation/theta6"]
+
 
 print("Building enviroment...")
 #Init server
-server = MqttClient()
+mqtt_client = MqttClient()
 
 #Init simulation enviroment
 env = swift.Swift()
 
 #Robot assign for simulation
-robot = Jaco2_erobot.KinovaJaco2()
+robot_real = Jaco2_erobot.KinovaJaco2()
+robot_simulation = Jaco2_erobot.KinovaJaco2()
 
 # Create an empty swift enviroment
 env.launch(realtime=True)
 
 #Theta values are set to their zero position
-robot.q = robot.qr
+robot_simulation.q = robot_simulation.qr
+robot_real.q = robot_real.qr
+
+#Varible for last value received 
+last_value_real = []
+last_value_simulation = []
 
 #Add the robot model to Swift
-main_robot = env.add(robot)
+real_robot = env.add(robot_real)
+simulation_robot = env.add(robot_simulation, robot_alpha=0.4)
 
 #Trajectory values between trajectory visualized 
-traj = 1
+traj = 2
 
 #Time step in seconds for update of the the graphical scene
 try:
@@ -43,23 +73,53 @@ else:
 
 print("Simulation running...")
 
+def stop_handler(signal, frame):
+    print("Stopping program...")
+    for task in asyncio.all_tasks():
+        task.cancel()
+    sys.exit(0)
+
+
 while True:
-    # Animate the robot
+     # Animate the robot
     env.step()
 
-    current_position = server.subscribe_and_wait_for_messages()
-    print(current_position)
-    robot.q = [current_position[0],current_position[1],current_position[2],current_position[3],current_position[4],current_position[5],
-               0,0,0,0,0,0]
+    simulation_values = mqtt_client.subscribe_and_wait_for_messages(simulation_topics)
+    if simulation_values != None:
+        finger1_theta = 0 #(simulation_values[6]/100)*np.pi/180
+        finger2_theta = 0 #(simulation_values[7]/100)*np.pi/180
+        finger3_theta = 0 #(simulation_values[8]/100)*np.pi/180
+        if last_value_simulation != simulation_values:
+            print(simulation_values)
 
-    #Checks if the trajectory should be visualized
-    if trajectory == True:
-        if traj == 0:
-            traj_visual = Jaco2_erobot.KinovaJaco2()
-            traj_visual.q = robot.q
-            env.add(traj_visual, robot_alpha=0.2)
-            traj = 1
-        else:
-            traj = traj-1
+        robot_simulation.q = [simulation_values[0]*np.pi/180,simulation_values[1]*np.pi/180,simulation_values[2]*np.pi/180,simulation_values[3]*np.pi/180,simulation_values[4]*np.pi/180,simulation_values[5]*np.pi/180,
+                finger1_theta,0,finger2_theta,0,finger3_theta,0]
+        
+        #Set the last value as last_value_simulation
+        last_value_simulation = simulation_values
+
+        #Checks if the trajectory should be visualized
+        if trajectory == True:
+            if traj == 0:
+                traj_visual = Jaco2_erobot.KinovaJaco2()
+                traj_visual.q = robot_simulation.q
+                env.add(traj_visual, robot_alpha=0.2)
+                traj = 2
+            else:
+                traj = traj-1
+
+    current_position_robot = mqtt_client.subscribe_and_wait_for_messages(current_theta_topics)
+    if current_position_robot != None:
+        finger1_theta = (current_position_robot[6]/100)*np.pi/180
+        finger2_theta = (current_position_robot[7]/100)*np.pi/180
+        finger3_theta = (current_position_robot[8]/100)*np.pi/180
+        if last_value_real != current_position_robot:
+            print(current_position_robot)
+
+        robot_real.q = [current_position_robot[0]*np.pi/180,current_position_robot[1]*np.pi/180,current_position_robot[2]*np.pi/180,current_position_robot[3]*np.pi/180,current_position_robot[4]*np.pi/180,current_position_robot[5]*np.pi/180,
+                    finger1_theta,0,finger2_theta,0,finger3_theta,0]
+
+        #Set the last value as last_value_real
+        last_value_real = current_position_robot
 
 
